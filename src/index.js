746bf7606a61094333b7fba1798a6c5a6ddc0875
romantic <cesc_fabregas1414@hotmail.com>
2019-10-02 15:33:27 +0700

    [init] witout deploy configs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 const root: ?Element = document.getElementById("root");
 
 if (root != null) {
   ReactDOM.render(<App />, root);
-  serviceWorker();
+  // serviceWorker();
 }
